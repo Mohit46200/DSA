0709dd75b478f91f7c17a1ae9c20356a870dbad7
Mohit46200 <mohit22600@gmail.com>
2026-08-26 12:46:35 +0530

    sorting method

--- MajorityElement.cpp
+++ MajorityElement.cpp
@@ -1,8 +1,9 @@
 #include <iostream> 
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int MajorityElement(vector<int>& nums){        //Moor's voting Algorithum
+int MoorsVoting(vector<int>& nums){        //Moor's voting Algorithum
         int freq = 0;
         int answer = nums[0];
         for(int i = 0 ; i<nums.size(); i++){
@@ -17,8 +18,27 @@ int MajorityElement(vector<int>& nums){        //Moor's voting Algorithum
         return answer;
 }
 
+int sortingMethod(vector<int>& nums){
+    sort(nums.begin(),nums.end());
+    int freq = 1;
+    int ans = nums[0];
+    for(int i=1;i<nums.size();i++){
+        if(nums[i] == nums[i-1]){
+            freq++;
+        }else{
+            freq=1;
+            ans = nums[i];
+        }
+        if(freq>nums.size()/2){
+            return ans;
+        }
+    }
+    return -1;
+}
+
 int main() {
     vector<int> nums = {1,2,56,45,2,2,2,2,2};
-    cout<<MajorityElement(nums)<<"\n";
+    cout<<MoorsVoting(nums)<<"\n";
+    cout<<sortingMethod(nums)<<"\n";
    
 }
